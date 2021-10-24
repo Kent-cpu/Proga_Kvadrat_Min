e2ee865f4516c0f616ce9fe9a07ace92fc5c5549
Руслан Мирзаев <ruslanmirzaev16621@gmail.com>
2021-10-24 23:53:02 +0700

    Ready

---
+++ Proga_lab_3/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿
+namespace Proga_lab_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.calculateBtnN1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.calculateBtnN2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.googleSheetsBtn = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataExcelBtn = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitBtn = new System.Windows.Forms.ToolStripMenuItem();
+            this.answerField = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.xColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.yColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.calculateBtnN1,
+            this.calculateBtnN2,
+            this.googleSheetsBtn,
+            this.dataExcelBtn,
+            this.exitBtn});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1200, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // calculateBtnN1
+            // 
+            this.calculateBtnN1.Name = "calculateBtnN1";
+            this.calculateBtnN1.Size = new System.Drawing.Size(164, 24);
+            this.calculateBtnN1.Text = "Рассчитать для n = 1";
+            this.calculateBtnN1.Click += new System.EventHandler(this.calculateBtnN1_Click_1);
+            // 
+            // calculateBtnN2
+            // 
+            this.calculateBtnN2.Name = "calculateBtnN2";
+            this.calculateBtnN2.Size = new System.Drawing.Size(164, 24);
+            this.calculateBtnN2.Text = "Рассчитать для n = 2";
+            this.calculateBtnN2.Click += new System.EventHandler(this.calculateBtnN2_Click);
+            // 
+            // googleSheetsBtn
+            // 
+            this.googleSheetsBtn.Name = "googleSheetsBtn";
+            this.googleSheetsBtn.Size = new System.Drawing.Size(119, 24);
+            this.googleSheetsBtn.Text = "Google Sheets";
+            this.googleSheetsBtn.Click += new System.EventHandler(this.googleSheetsBtn_Click);
+            // 
+            // dataExcelBtn
+            // 
+            this.dataExcelBtn.Name = "dataExcelBtn";
+            this.dataExcelBtn.Size = new System.Drawing.Size(57, 24);
+            this.dataExcelBtn.Text = "Excel";
+            this.dataExcelBtn.Click += new System.EventHandler(this.dataExcel_Click);
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(67, 24);
+            this.exitBtn.Text = "Выход";
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
+            // answerField
+            // 
+            this.answerField.Location = new System.Drawing.Point(38, 65);
+            this.answerField.Name = "answerField";
+            this.answerField.Size = new System.Drawing.Size(318, 22);
+            this.answerField.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Ответ";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.xColumn,
+            this.yColumn});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 121);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(402, 258);
+            this.dataGridView1.TabIndex = 11;
+            // 
+            // xColumn
+            // 
+            this.xColumn.HeaderText = "X";
+            this.xColumn.MinimumWidth = 6;
+            this.xColumn.Name = "xColumn";
+            this.xColumn.Width = 125;
+            // 
+            // yColumn
+            // 
+            this.yColumn.HeaderText = "Y";
+            this.yColumn.MinimumWidth = 6;
+            this.yColumn.Name = "yColumn";
+            this.yColumn.Width = 125;
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.Location = new System.Drawing.Point(532, 121);
+            this.zedGraphControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0D;
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.Size = new System.Drawing.Size(642, 421);
+            this.zedGraphControl1.TabIndex = 12;
+            this.zedGraphControl1.UseExtendedPrintDialog = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 609);
+            this.Controls.Add(this.zedGraphControl1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.answerField);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem calculateBtnN1;
+        private System.Windows.Forms.ToolStripMenuItem googleSheetsBtn;
+        private System.Windows.Forms.ToolStripMenuItem dataExcelBtn;
+        private System.Windows.Forms.TextBox answerField;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn xColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn yColumn;
+        private System.Windows.Forms.ToolStripMenuItem exitBtn;
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+        private System.Windows.Forms.ToolStripMenuItem calculateBtnN2;
+    }
+}
+
---
+++ Proga_lab_3/Form1.cs
@@ -0,0 +1,358 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Collections.Generic;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Auth.OAuth2;
+using System.IO;
+using Google.Apis.Services;
+using MathNet.Numerics.LinearAlgebra;
+using ZedGraph;
+using System.Threading;
+
+
+namespace Proga_lab_3
+{
+    public partial class Form1 : Form
+    {
+        List<Point> points = new List<Point>();
+        static readonly string[] Scopes = { SheetsService.Scope.Spreadsheets };
+        static readonly string SpreadsheetId = "1BbjZxzIjTQ1ACC0q3kYqXJl-0RChhXQe_viwL7tF1PI";
+        static readonly string sheet = "Data";
+        static SheetsService service;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void disbledBtn(bool enable)
+        {
+            dataExcelBtn.Enabled = enable;
+            calculateBtnN1.Enabled = enable;
+            calculateBtnN2.Enabled = enable;
+            googleSheetsBtn.Enabled = enable;
+        }
+
+        public void drawN2(double a, double b, double c)
+        {
+            GraphPane pane = zedGraphControl1.GraphPane;
+            pane.CurveList.Clear();
+
+            PointPairList list = new PointPairList();
+            for (int i = 0; i < points.Count; ++i)
+            {
+                list.Add(points[i].x, points[i].y);
+            }
+            double minX = findPoint()[0];
+            double maxX = findPoint()[1];
+
+
+            LineItem myCurve = pane.AddCurve("Points", list, Color.Blue, SymbolType.Diamond);
+            LineItem approximating = pane.AddCurve("f(x)", new double[] { minX, maxX },
+                new double[] { a * minX * minX + b * minX + c, a * maxX * maxX + b * maxX + c }, Color.DarkRed, SymbolType.Circle);
+
+            myCurve.Line.IsVisible = false;
+
+            myCurve.Symbol.Fill.Color = Color.Blue;
+            myCurve.Symbol.Fill.Type = FillType.Solid;
+
+            myCurve.Symbol.Size = 7;
+
+            zedGraphControl1.AxisChange();
+
+            zedGraphControl1.Invalidate();
+        }
+
+        private void drawN1(double a, double b)
+        {
+            GraphPane pane = zedGraphControl1.GraphPane;
+            pane.CurveList.Clear();
+
+            PointPairList list = new PointPairList();
+            for (int i = 0; i < points.Count; ++i)
+            {
+                list.Add(points[i].x, points[i].y);
+            }
+
+
+            LineItem myCurve = pane.AddCurve("Points", list, Color.Blue, SymbolType.Diamond);
+            LineItem approximating = pane.AddCurve("f(x)", new double[] { findPoint()[0], findPoint()[1] }, new double[] { a * findPoint()[0] + b, a * findPoint()[1] + b }, Color.DarkRed, SymbolType.Circle);
+
+            myCurve.Line.IsVisible = false;
+
+            myCurve.Symbol.Fill.Color = Color.Blue;
+            myCurve.Symbol.Fill.Type = FillType.Solid;
+
+            myCurve.Symbol.Size = 7;
+
+            zedGraphControl1.AxisChange();
+
+            zedGraphControl1.Invalidate();
+        }
+
+
+        private void dataExcel_Click(object sender, EventArgs e)
+        {
+            new Thread(() =>
+            {
+                try
+                {
+
+                    Invoke((System.Action)(() =>
+                    {
+                        disbledBtn(false);
+                        points.Clear();
+                        dataGridView1.Rows.Clear();
+                    }));
+                    
+
+                    string path = @"C:\Users\rusla\Desktop\Data.xlsx";
+                    Excel.Application ObjExcel = new Excel.Application();
+                    Workbook ObjWorkBook = ObjExcel.Workbooks.Open(path);
+                    Worksheet ObjWorkSheet = (Worksheet)ObjWorkBook.Sheets[1];
+
+                    Range xRange = ObjWorkSheet.UsedRange.Columns[1];
+                    Range yRange = ObjWorkSheet.UsedRange.Columns[2];
+                    Array xCells = (Array)xRange.Cells.Value2;
+                    Array yCells = (Array)yRange.Cells.Value2;
+
+                    string[] xColumn = xCells.OfType<object>().Select(o => o.ToString()).ToArray();
+                    string[] yColumn = yCells.OfType<object>().Select(o => o.ToString()).ToArray();
+
+                    for (int i = 0; i < xColumn.Length; ++i)
+                    {
+                        Invoke((System.Action)(() =>
+                        {
+                            dataGridView1.Rows.Add(xColumn[i], yColumn[i]);
+                        }));
+                        
+                    }
+                    ObjWorkBook.Close();
+                    ObjExcel.Quit();
+
+                    Invoke((System.Action)(() =>
+                    {
+                        disbledBtn(true);
+                    }));
+
+                }
+                catch (Exception error)
+                {
+                    MessageBox.Show("Ошибка при считывания данных");
+                }
+            }).Start();
+            
+
+        }
+
+        private void googleSheetsBtn_Click(object sender, EventArgs e)
+        {
+            new Thread(() =>
+            {
+                try
+                {
+                    Invoke((System.Action)(() =>
+                        {
+                            disbledBtn(false);
+                            points.Clear();
+                            dataGridView1.Rows.Clear();
+                        }));
+
+                    GoogleCredential credential;
+                    using (var stream = new FileStream("creds.json", FileMode.Open, FileAccess.Read))
+                    {
+                        credential = GoogleCredential.FromStream(stream)
+                            .CreateScoped(Scopes);
+                    }
+
+                    service = new SheetsService(new BaseClientService.Initializer()
+                    {
+                        HttpClientInitializer = credential,
+                    });
+
+                    var range = $"Point!A:B";
+                    SpreadsheetsResource.ValuesResource.GetRequest request = service.Spreadsheets.Values.Get(SpreadsheetId, range);
+
+                    var response = request.Execute();
+                    IList<IList<object>> values = response.Values;
+
+                    if (values != null && values.Count > 0)
+                    {
+                        foreach (var row in values)
+                        {
+                            Invoke((System.Action)(() =>
+                            {
+                                dataGridView1.Rows.Add(row[0], row[1]);
+                            }));
+                        }
+                    }
+
+                    Invoke((System.Action)(() =>
+                    {
+                        disbledBtn(true);
+                    }));
+
+
+                }
+                catch (Exception error)
+                {
+                    MessageBox.Show("Ошибка при считывания данных");
+                }
+            }).Start();
+        }
+
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void calculateBtnN2_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                points.Clear();
+                for (int i = 0; i < dataGridView1.Rows.Count; ++i)
+                {
+                    Point point = new Point(Convert.ToDouble(dataGridView1[0, i].Value), Convert.ToDouble(dataGridView1[1, i].Value));
+                    points.Add(point);
+                }
+
+                if (points.Count > 0)
+                {
+                    double XiPow4 = 0, XiPow3 = 0, XiPow2 = 0, Xi = 0, XiMultiYi = 0, XiPow2MultiYi = 0, Yi = 0, n = points.Count;
+
+                    for (int i = 0; i < points.Count; ++i)
+                    {
+                        XiPow4 += Math.Pow(points[i].x, 4);
+                        XiPow3 += Math.Pow(points[i].x, 3);
+                        XiPow2 += Math.Pow(points[i].x, 2);
+                        XiMultiYi += points[i].x * points[i].y;
+                        XiPow2MultiYi += Math.Pow(points[i].x, 2) * points[i].y;
+                        Xi += points[i].x;
+                        Yi += points[i].y;
+                    }
+
+                    Matrix<double> A = Matrix<double>.Build.DenseOfArray(new double[,] {
+                        { XiPow4, XiPow3, XiPow2},
+                        { XiPow3, XiPow2, Xi },
+                        { XiPow2, Xi, n},
+                    });
+
+                    try
+                    {
+                        Vector<double> B = Vector<double>.Build.Dense(new double[] { XiPow2MultiYi, XiMultiYi, Yi });
+                        var result = A.SolveIterative(B, new MathNet.Numerics.LinearAlgebra.Double.Solvers.MlkBiCgStab());
+                        answerField.Text = "a: " + Math.Round(result[0], 2).ToString() + " b: " + Math.Round(result[1], 2).ToString() + " c:" + Math.Round(result[2], 2).ToString();
+                        drawN2(Math.Round(result[0], 2), Math.Round(result[1], 2), Math.Round(result[2], 2)); 
+                    }
+                    catch (Exception error)
+                    {
+                        MessageBox.Show("Ошибка в вычисления");
+                        answerField.Text = null;
+                        return;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Ведите данные");
+                }
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show("Ошибки в вычислениях");
+            }
+        }
+
+        private void calculateBtnN1_Click_1(object sender, EventArgs e)
+        {
+           
+            try
+            {
+                points.Clear();
+                for (int i = 0; i < dataGridView1.Rows.Count - 1; ++i)
+                {
+                    Point point = new Point(Convert.ToDouble(dataGridView1[0, i].Value), Convert.ToDouble(dataGridView1[1, i].Value));
+                    points.Add(point);
+                }
+                if (points.Count > 0)
+                {
+                    double XiPow2 = 0, Xi = 0, XiMultiYi = 0, Yi = 0, n = points.Count;
+                    for (int i = 0; i < points.Count; ++i)
+                    {
+                        XiPow2 += Math.Pow(points[i].x, 2);
+                        Xi += points[i].x;
+                        XiMultiYi += points[i].x * points[i].y;
+                        Yi += points[i].y;
+                    }
+
+                    Matrix<double> A = Matrix<double>.Build.DenseOfArray(new double[,] {
+                    { XiPow2, Xi},
+                    { Xi, n },
+                });
+
+                    Vector<double> B = Vector<double>.Build.Dense(new double[] { XiMultiYi, Yi });
+
+                    var result = A.SolveIterative(B, new MathNet.Numerics.LinearAlgebra.Double.Solvers.MlkBiCgStab());
+                    answerField.Text = "a: " + Math.Round(result[0], 2).ToString() + " b: " + Math.Round(result[1], 2).ToString();
+
+                    drawN1(Math.Round(result[0], 2), Math.Round(result[1], 2));
+                }
+                else
+                {
+                    MessageBox.Show("Ведите данные");
+                }
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show("Ошибки в вычислениях");
+            }
+
+
+        }
+
+        private double[] findPoint()
+        {
+            double minElement = points[0].x;
+            double maxElement = points[0].x;
+            for (int i = 0; i < points.Count; i++)
+            {
+                if (points[i].x < minElement)
+                {
+                    minElement = points[i].x;
+                }
+                else if (maxElement < points[i].x)
+                {
+                    maxElement = points[i].x;
+                }
+            }
+            double[] result = { minElement, maxElement };
+            return result;
+        }
+    }
+}
+
+
+class Point
+{
+    public double x, y;
+
+    public Point()
+    {
+
+    }
+
+    public Point(double xKoordinate, double yKoordinate)
+    {
+        x = xKoordinate;
+        y = yKoordinate;
+    }
+}
+
